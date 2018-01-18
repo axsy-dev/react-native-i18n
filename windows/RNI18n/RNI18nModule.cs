1cc3a68ddb138d876fc624499e9c8fdcf6fee37d
Artem Pechenyi <pechkinator@gmail.com>
2018-01-18 20:59:00 +0200

    Windows support

---
+++ windows/RNI18n/RNI18nModule.cs
@@ -0,0 +1,42 @@
+﻿using Newtonsoft.Json.Linq;
+using ReactNative.Bridge;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RNI18n
+{
+    public class RNI18nModule : ReactContextNativeModuleBase
+    {
+        public RNI18nModule(ReactContext reactContext) : base(reactContext)
+        {
+        }
+
+        public override string Name => "RNI18n";
+
+        public override IReadOnlyDictionary<string, object> Constants
+        {
+            get
+            {
+                return new Dictionary<string, object>
+                {
+                    { "languages", Windows.System.UserProfile.GlobalizationPreferences.Languages }
+                };
+            }
+        }
+
+        [ReactMethod]
+        public void getLanguages(IPromise promise)
+        {
+            var jarr = new JArray();
+            IReadOnlyList<string> userLanguages = Windows.System.UserProfile.GlobalizationPreferences.Languages;
+            foreach (var userLanguage in userLanguages)
+            {
+                jarr.Add(userLanguage);
+            }
+            promise.Resolve(jarr);
+        }
+    }
+}
---
+++ windows/RNI18n/RNI18nPackage.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ReactNative.Bridge;
+using ReactNative.Modules.Core;
+using ReactNative.UIManager;
+
+namespace RNI18n
+{
+    public class RNI18nPackage : IReactPackage
+    {
+        /// <summary>
+        /// Creates the list of native modules to register with the react
+        /// instance.
+        /// </summary>
+        /// <param name="reactContext">The react application context.</param>
+        /// <returns>The list of native modules.</returns>
+        public IReadOnlyList<INativeModule> CreateNativeModules(ReactContext reactContext)
+        {
+            return new List<INativeModule>
+            {
+                new RNI18nModule(reactContext),
+            };
+        }
+
+        /// <summary>
+        /// Creates the list of JavaScript modules to register with the
+        /// react instance.
+        /// </summary>
+        /// <returns>The list of JavaScript modules.</returns>
+        public IReadOnlyList<Type> CreateJavaScriptModulesConfig()
+        {
+            return new List<Type>(0);
+        }
+
+        /// <summary>
+        /// Creates the list of view managers that should be registered with
+        /// the <see cref="UIManagerModule"/>.
+        /// </summary>
+        /// <param name="reactContext">The react application context.</param>
+        /// <returns>The list of view managers.</returns>
+        public IReadOnlyList<IViewManager> CreateViewManagers(
+            ReactContext reactContext)
+        {
+            return new List<IViewManager>(0);
+        }
+    }
+}
