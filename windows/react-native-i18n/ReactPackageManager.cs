026d264988bdb3d265b8d75f7a258a6cf46510aa
gillpeacegood <gill.peacegood@axsy.com>
2020-10-16 12:33:10 +0100

    Makes this work on 0.63

--- windows/react-native-i18n/ReactPackageManager.cs
+++ windows/react-native-i18n/ReactPackageManager.cs
@@ -3,11 +3,13 @@ using Microsoft.ReactNative.Managed;
 
 namespace react_native_i18n
 {
-  public sealed class ReactPackageProvider : IReactPackageProvider
-  {
-    public void CreatePackage(IReactPackageBuilder packageBuilder)
+    public sealed class ReactPackageProvider : IReactPackageProvider
     {
-      packageBuilder.AddAttributedModules();
+        public void CreatePackage(IReactPackageBuilder packageBuilder)
+        {
+            packageBuilder.AddReflectionReactPackageProvider<ReactPackageProvider>();
+        }
+
+
     }
-  }
 }
